44a10bad5327324a7c6f1242e9a061affa3ae3f6
shijith pk <shijith.thalassery@gmail.com>
2023-07-18 13:13:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const [editTaskId, setEditTaskId] = useState('');
+
+  const deleteTask = (id) => {
+    setTodos(toDos.filter((task) => task.id !== id));
+  };
+
+  const addTask = () => {
+    const trimmedInput = toDo.trim();
+    if (trimmedInput.length === 0) {
+      return;
+    }
+
+    if (editMode) {
+      setTodos(
+        toDos.map((task) => {
+          if (task.id === editTaskId) {
+            task.text = trimmedInput;
+          }
+          return task;
+        })
+      );
+      setEditMode(false);
+      setEditTaskId('');
+    } else {
+      setTodos([...toDos, { id: Date.now(), text: trimmedInput, status: false }]);
+    }
+
+    setTodo('');
+  };
+
+  const editTask = (id) => {
+    const taskToEdit = toDos.find((task) => task.id === id);
+    if (taskToEdit) {
+      setEditMode(true);
+      setEditTaskId(id);
+      setTodo(taskToEdit.text);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {new Date().toLocaleDateString('en-US', { weekday: 'long' })} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(event) => setTodo(event.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTask} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((task) => (
+          <div className="todo" key={task.id}>
+            <div className="left">
+              <input
+                onChange={(event) =>
+                  setTodos(
+                    toDos.map((t) => {
+                      if (t.id === task.id) {
+                        t.status = event.target.checked;
+                      }
+                      return t;
+                    })
+                  )
+                }
+                checked={task.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{task.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-pen" onClick={() => editTask(task.id)}></i>
+              <i className="fas fa-times" onClick={() => deleteTask(task.id)}></i>
+            </div>
+          </div>
+        ))}
+        {toDos
+          .filter((task) => task.status)
+          .map((completedTask) => (
+            <h1 key={completedTask.id}>{completedTask.text}</h1>
+          ))}
+      </div>
     </div>
   );
 }
